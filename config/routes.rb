1cbe52a362b23e11c387a3e99f55d15339462e06
tanagoda <tanagoda.tech@gmail.com>
2019-03-02 21:06:08 +0000

    チーム紹介ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get '/home', to: 'home#index'
+  get '/team', to: 'team#index'
 
   get '*not_found' => 'errors#routing_error'
   post '*not_found' => 'errors#routing_error'
