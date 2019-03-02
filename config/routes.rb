cd56dfb5132d0574a006d29948ce3c54ecb2b8ff
tanagoda <tanagoda.tech@gmail.com>
2019-03-02 21:08:09 +0000

    scheduleページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/home', to: 'home#index'
   get '/team', to: 'team#index'
+  get '/schedule', to: 'schedule#index'
 
   get '*not_found' => 'errors#routing_error'
   post '*not_found' => 'errors#routing_error'
