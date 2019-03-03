139c0be50472f995871110aa1c499437792241c6
tanagoda <tanagoda.tech@gmail.com>
2019-03-03 07:37:14 +0000

    ユーザーモデルを追加 bundle exec rails g devise User bundle exec rails db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
   get '/home', to: 'home#index'
   get '/team', to: 'team#index'
   get '/schedule', to: 'schedule#index'
+
+  devise_for :users
 
   get '*not_found' => 'errors#routing_error'
   post '*not_found' => 'errors#routing_error'
